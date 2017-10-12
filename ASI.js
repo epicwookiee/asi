64993600370a0ff2ee809e867b6e851d606d870e
Aron Aebersold <epicwookiee@gmail.com>
2017-10-12 18:42:48 -0400

    Tables work
    tab links now open table

--- ASI.js
+++ ASI.js
@@ -1,3 +1,14 @@
 //Detect correct username and password
 //var psw=document.getElementById('psw');
 //if((psw.value=="password"));
+function openNav() {
+  document.getElementById("sideNav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav () {
+  document.getElementById("sideNav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
+}
