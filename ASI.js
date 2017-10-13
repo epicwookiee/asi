4f408751fa00a24f17814f77a4e63931e1dc8a61
Aron Aebersold <epicwookiee@gmail.com>
2017-10-12 20:01:40 -0400

    Side nav working
    side nav working, need to fix css

--- ASI.js
+++ ASI.js
@@ -2,13 +2,14 @@
 //var psw=document.getElementById('psw');
 //if((psw.value=="password"));
 function openNav() {
-  document.getElementById("sideNav").style.width = "250px";
+  document.getElementById("mySideNav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav () {
-  document.getElementById("sideNav").style.width = "0";
+  document.getElementById("mySideNav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
   document.body.style.backgroundColor = "white";
 }
+$('#mySideNav').hide();
