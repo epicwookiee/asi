8819f608fdae82239e74edd28d3c429dcc40945e
Aron Aebersold <epicwookiee@gmail.com>
2017-10-19 17:57:59 -0400

    version 1.0 side name
    side nav working properly, tables not shifting right.

--- ASI.js
+++ ASI.js
@@ -1,15 +1,12 @@
-//Detect correct username and password
-//var psw=document.getElementById('psw');
-//if((psw.value=="password"));
 function openNav() {
   document.getElementById("mySideNav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementByTagName("table").style.marginLeft = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav () {
   document.getElementById("mySideNav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+  document.getElementByTagName("table").style.marginLeft = "0";
   document.body.style.backgroundColor = "white";
 }
 $('#mySideNav').hide();
